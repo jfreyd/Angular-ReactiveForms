7bfdeffec62c2516f43b5ab898554e737e7a04a8
Joseph Freydlin <joseph.freydlin@sprint.com>
2019-04-29 09:25:28 -0500

    Added timeout using debounceTime

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms'
+import { debounceTime } from 'rxjs/operators';
 
 import { Customer } from './customer';
 
@@ -66,7 +67,7 @@ export class CustomerComponent implements OnInit {
      );
 
      const emailControl = this.customerForm.get('emailGroup.email');
-      emailControl.valueChanges.subscribe(
+      emailControl.valueChanges.pipe(debounceTime(1000)).subscribe(
         value => this.setMessage(emailControl)
       );
   }
